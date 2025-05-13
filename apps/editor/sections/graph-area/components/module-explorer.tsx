b228140282f19fc14a348311dfadef13d2f8339d
Rafael Avila Martinez <rafaelmartinez@etsy.com>
2025-05-13 16:39:05 -0400

    add mock card to show module details

--- apps/editor/sections/graph-area/components/module-explorer.tsx
+++ apps/editor/sections/graph-area/components/module-explorer.tsx
@@ -1,18 +1,22 @@
 import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
+import { Card, CardBody, CardFooter, CardHeader, Divider, Link } from "@heroui/react";
 import { Panel } from "@xyflow/react";
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 
 const modules = [
-    {
-        label: "Whale",
-        key: "whale",
-        description: "Diverse group of fully aquatic placental marine mammals",
-    },
-    { label: "Otter", key: "otter", description: "A carnivorous mammal in the subfamily Lutrinae" },
-    { label: "Crocodile", key: "crocodile", description: "A large semiaquatic reptile" },
+    { name: "Whale", key: "whale", description: "Diverse group of fully aquatic placental marine mammals" },
+    { name: "Otter", key: "otter", description: "A carnivorous mammal in the subfamily Lutrinae" },
+    { name: "Crocodile", key: "crocodile", description: "A large semiaquatic reptile" },
 ];
 
 export default function ModuleExplorer() {
+
+    const [selectedModule, setSelectedModule] = useState(null);
+
+    const onModuleSelectionChange = (key: String | null) => {
+        const selected = modules.find((module) => module.key === key);
+        setSelectedModule(selected);
+    };
 
     return (
         <Panel
@@ -25,13 +29,35 @@ export default function ModuleExplorer() {
                 'backgroundColor': 'rgba(255, 255, 255, 0.8)',
                 'height': 'auto',
             } as CSSProperties}>
-            <div>
-                <Autocomplete size="sm" label="Modules" placeholder="Search for your module">
+            <div className="flex flex-col gap-2">
+                <Autocomplete size="sm" label="Modules" placeholder="Search for your module"
+                    onSelectionChange={onModuleSelectionChange}>
                     {modules.map((animal) => (
-                        <AutocompleteItem key={animal.key}>{animal.label}</AutocompleteItem>
+                        <AutocompleteItem key={animal.key}>{animal.name}</AutocompleteItem>
                     ))}
                 </Autocomplete>
             </div>
+            {selectedModule && (
+                <div className="flex flex-col gap-2 mt-2 pt-2">
+                    <Card className="max-w-full">
+                        <CardHeader className="flex gap-3">
+                            <div className="flex flex-col">
+                                <p className="text-md">HeroUI</p>
+                            </div>
+                        </CardHeader>
+                        <Divider />
+                        <CardBody>
+                            <p>Make beautiful websites regardless of your design experience.</p>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter>
+                            <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+                                Visit source code on GitHub.
+                            </Link>
+                        </CardFooter>
+                    </Card>
+                </div>
+            )}
         </Panel>
 
     );
